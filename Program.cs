acbe73f6835f68152f113833751905a5a6d17708
malagi <malagi@gmail.com>
2020-01-26 17:49:44 +0100

    A fő eljárások megtervezése

--- Program.cs
+++ Program.cs
@@ -10,8 +10,35 @@ namespace Gites
     {
         static void Main(string[] args)
         {
-
+            char[,] pálya;
+            PályaInit(out pálya); //Feltölti aláhúzás jelekkel a pályát
+            Aknázás(pálya);
+            Kirajzolás(pálya);
             Console.ReadKey();
+        }
+
+        private static void Kirajzolás(char[,] pálya)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Aknázás(char[,] pálya)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PályaInit(out char[,] pálya)
+        {
+            Console.Write("Kérem adja meg, hogy mekkora pályán akar játszani: (5-20)");
+            int méret = int.Parse(Console.ReadLine());
+            pálya = new char[méret, méret];
+            for (int i = 0; i < pálya.GetLength(0); i++)
+            {
+                for (int j = 0; j < pálya.GetLength(1); j++)
+                {
+                    pálya[i, j] = '_';
+                }
+            }
         }
     }
 }
